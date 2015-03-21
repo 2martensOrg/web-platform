d634e397999a12731a88b7272a13df1ba655f917
Jim Martens <github@2martens.de>
2015-03-21 11:17:20 +0100

    [CoreBundle] Added test infrastructure

---
+++ src/TwoMartens/Bundle/CoreBundle/Tests/Functional/ACPDashboardControllerTest.php
@@ -0,0 +1,29 @@
+<?php
+/*
+ * This file is part of the 2martens Web Platform.
+ *
+ * (c) Jim Martens <github@2martens.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace TwoMartens\Bundle\CoreBundle\Tests\Functional;
+
+/**
+ * Tests the ACPDashboardController.
+ *
+ * @author Jim Martens <github@2martens.de>
+ */
+class ACPDashboardControllerTest extends WebTestCase
+{
+    public function testDashboard()
+    {
+        $client = $this->createClient(array('test_case' => 'ACPDashboard', 'root_config' => 'config.yml'));
+        $crawler = $client->request('GET', '/acp');
+        $html = $crawler->html();
+
+        $containsExpected = (strpos($html, '<p class="alert alert-info" role="alert">') !== -1);
+        $this->assertTrue($containsExpected);
+    }
+}
