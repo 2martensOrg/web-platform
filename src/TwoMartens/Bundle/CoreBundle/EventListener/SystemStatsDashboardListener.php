834ab8709c5227c6514a9de134109d5ed5cb8965
Jim Martens <github@2martens.de>
2015-05-09 13:42:08 +0200

    [CoreBundle] Moved event listener to EventListener directory

--- src/TwoMartens/Bundle/CoreBundle/EventListener/SystemStatsDashboardListener.php
+++ src/TwoMartens/Bundle/CoreBundle/EventListener/SystemStatsDashboardListener.php
@@ -7,7 +7,7 @@
  * file that was distributed with this source code.
  */
 
-namespace TwoMartens\Bundle\CoreBundle\Event;
+namespace TwoMartens\Bundle\CoreBundle\EventListener;
 
 use Sonata\BlockBundle\Event\BlockEvent;
 use Sonata\BlockBundle\Model\Block;
