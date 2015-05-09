9e1101b1f7f4b2b92559bc721938470599047162
Jim Martens <github@2martens.de>
2015-05-09 15:18:00 +0200

    [CoreBundle] Implemented Field class
    * closes #23

---
+++ src/TwoMartens/Bundle/CoreBundle/Model/Field/DefaultField.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+ * (c) Jim Martens <github@2martens.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace TwoMartens\Bundle\CoreBundle\Model\Field;
+
+
+/**
+ * Represents a generic field.
+ * 
+ * @author    Jim Martens <github@2martens.de>
+ * @copyright 2013-2015 Jim Martens
+ */
+class DefaultField extends AbstractField
+{
+
+}
