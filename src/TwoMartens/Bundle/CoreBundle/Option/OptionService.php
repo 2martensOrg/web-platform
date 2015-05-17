98071d16cb81c2bb4081439be6a4431f6aa1ebc0
Jim Martens <github@2martens.de>
2015-05-17 11:36:41 +0200

    [CoreBundle] Added OptionService
    * closes #40

---
+++ src/TwoMartens/Bundle/CoreBundle/Option/OptionService.php
@@ -0,0 +1,163 @@
+<?php
+
+/*
+ * (c) Jim Martens <github@2martens.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace TwoMartens\Bundle\CoreBundle\Option;
+
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\Finder\Finder;
+use Symfony\Component\Finder\SplFileInfo;
+use Symfony\Component\Yaml\Dumper;
+use Symfony\Component\Yaml\Parser;
+use TwoMartens\Bundle\CoreBundle\Model\Option\Option;
+use TwoMartens\Bundle\CoreBundle\Model\Option\OptionCategory;
+use TwoMartens\Bundle\CoreBundle\Util\ConfigUtil;
+
+/**
+ * Provides core option functionality.
+ *
+ * @author    Jim Martens <github@2martens.de>
+ * @copyright 2013-2015 Jim Martens
+ */
+class OptionService
+{
+    /**
+     * the finder
+     * @var Finder
+     */
+    private $finder;
+
+    /**
+     * the YAML parser
+     * @var Parser
+     */
+    private $parser;
+
+    /**
+     * the YAML dumper
+     * @var Dumper
+     */
+    private $dumper;
+
+    /**
+     * the filesystem
+     * @var Filesystem
+     */
+    private $filesystem;
+
+    /**
+     * the options in YAML format
+     * @var array
+     */
+    private $optionsYAML;
+
+    /**
+     * Initializes the service.
+     *
+     * @param Finder     $finder     Must be configured to read ONE config file
+     * @param Parser     $parser     A YAML parser
+     * @param Dumper     $dumper     A YAML dumper
+     * @param Filesystem $filesystem A filesystem
+     */
+    public function __construct(Finder $finder, Parser $parser, Dumper $dumper, Filesystem $filesystem)
+    {
+        $this->finder = $finder;
+        $this->parser = $parser;
+        $this->dumper = $dumper;
+        $this->filesystem = $filesystem;
+
+        $this->optionsYAML = $this->readConfig();
+        if ($this->optionsYAML === null) {
+            throw new \LogicException('The given finder isn\'t properly configured.');
+        }
+    }
+
+    /**
+     * Writes the options to the config file.
+     */
+    public function __destruct()
+    {
+        $this->writeConfig();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getOptions()
+    {
+        return ConfigUtil::convertToOptions($this->optionsYAML);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setOptions(OptionCategory $options)
+    {
+        $this->optionsYAML = ConfigUtil::convertToArray($options);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function get($optionCategory, $optionName)
+    {
+        $optionValue = $this->optionsYAML[$optionCategory][$optionName];
+
+        return ConfigUtil::convertToOption($optionName, $optionValue);
+    }
+
+    /**
+     * Dumps the given data into the given config file.
+     *
+     * ATTENTION: This method DOES NOT perform any sanitize actions. It
+     * overwrites the given file with the given data entirely.
+     *
+     * @param Dumper     $dumper     A YAML dumper
+     * @param Filesystem $filesystem A Filesystem object
+     * @param string     $configFile Must be a valid path of a YAML file
+     * @param array      $data       The config data for the config file
+     *
+     * @api
+     */
+    public function set($optionCategory, $optionName, Option $value)
+    {
+        $this->optionsYAML[$optionCategory][$optionName] = $value->getValue();
+    }
+
+    /**
+     * Reads the config file configured in the finder and returns the
+     * YAML parsed content for it.
+     *
+     * @return array|null
+     */
+    private function readConfig()
+    {
+        $configContents = null;
+        foreach ($this->finder as $configFile) {
+            /** @var SplFileInfo $configFile */
+            $configContents = $this->parser->parse($configFile->getContents());
+        }
+
+        return $configContents;
+    }
+
+    /**
+     * Dumps the options into the config file.
+     *
+     * ATTENTION: This method DOES NOT perform any sanitize actions. It
+     * overwrites the config file with the options entirely.
+     */
+    private function writeConfig()
+    {
+        $dumpedData = $this->dumper->dump($this->optionsYAML, 2);
+        foreach ($this->finder as $configFile) {
+            /** @var SplFileInfo $configFile */
+            $this->filesystem->dumpFile($configFile->getRealPath(), $dumpedData);
+        }
+    }
+}
---
+++ src/TwoMartens/Bundle/CoreBundle/Option/OptionServiceInterface.php
@@ -0,0 +1,60 @@
+<?php
+/*
+ * (c) Jim Martens <github@2martens.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace TwoMartens\Bundle\CoreBundle\Option;
+
+use TwoMartens\Bundle\CoreBundle\Model\Option\Option;
+use TwoMartens\Bundle\CoreBundle\Model\Option\OptionCategory;
+
+/**
+ * Defines the API of the OptionService.
+ *
+ * @author    Jim Martens <github@2martens.de>
+ * @copyright 2013-2015 Jim Martens
+ */
+interface OptionServiceInterface
+{
+    /**
+     * Returns the top level option category.
+     *
+     * @api
+     *
+     * @return OptionCategory
+     */
+    public function getOptions();
+
+    /**
+     * Sets the options.
+     *
+     * @param OptionCategory $options
+     *
+     * @api
+     */
+    public function setOptions(OptionCategory $options);
+
+    /**
+     * Returns the option belonging to the given option category and name.
+     *
+     * @param string $optionCategory
+     * @param string $optionName
+     *
+     * @api
+     *
+     * @return Option
+     */
+    public function get($optionCategory, $optionName);
+
+    /**
+     * Sets the option with the given name in the given category to the given value.
+     *
+     * @param string $optionCategory
+     * @param string $optionName
+     * @param Option $value
+     */
+    public function set($optionCategory, $optionName, Option $value);
+}
