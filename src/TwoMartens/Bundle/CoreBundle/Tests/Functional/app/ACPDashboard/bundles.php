78a4d9a2fa3167a8ec0b70080c638cdd9dbdce19
Jim Martens <github@2martens.de>
2015-06-12 21:08:33 +0200

    [CoreBundle] Fixed test env for functional tests
    * hopefully this does the trick
    
    Signed-off-by: Jim Martens <github@2martens.de>

--- src/TwoMartens/Bundle/CoreBundle/Tests/Functional/app/ACPDashboard/bundles.php
+++ src/TwoMartens/Bundle/CoreBundle/Tests/Functional/app/ACPDashboard/bundles.php
@@ -2,10 +2,12 @@
 
 use Symfony\Bundle\FrameworkBundle\FrameworkBundle;
 use Symfony\Bundle\FrameworkBundle\Tests\Functional\Bundle\TestBundle\TestBundle;
+use Symfony\Bundle\TwigBundle\TwigBundle;
 use TwoMartens\Bundle\CoreBundle\TwoMartensCoreBundle;
 
 return array(
-    new TwoMartensCoreBundle(),
     new FrameworkBundle(),
+    new TwigBundle(),
+    new TwoMartensCoreBundle(),
     new TestBundle(),
 );
