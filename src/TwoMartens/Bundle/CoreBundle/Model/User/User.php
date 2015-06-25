4bf0e4a259c47c2d4b6de6623db2286beaeeecc5
Jim Martens <github@2martens.de>
2015-06-25 15:56:43 +0200

    [CoreBundle] Added User model
    * closes #32
    
    Signed-off-by: Jim Martens <github@2martens.de>

---
+++ src/TwoMartens/Bundle/CoreBundle/Model/User/User.php
@@ -0,0 +1,39 @@
+<?php
+
+/*
+ * (c) Jim Martens <github@2martens.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace TwoMartens\Bundle\CoreBundle\Model\User;
+
+use FOS\UserBundle\Model\User as FOSUser;
+
+/**
+ * Represents a user.
+ *
+ * @author    Jim Martens <github@2martens.de>
+ * @copyright 2013-2015 Jim Martens
+ */
+class User extends FOSUser
+{
+    /**
+     * the salt for the password
+     * @var string|null
+     */
+    protected $salt;
+
+    /**
+     * Initializes the User object.
+     */
+    public function __construct()
+    {
+        // saves salt into user object
+        // if bcrypt is used (default), salt should be null
+        parent::__construct();
+
+        $this->salt = null;
+    }
+}
