6e366b6a88ec12cb508c7c66aefc1489b9b45f74
Jim Martens <github@2martens.de>
2015-09-17 15:45:15 +0200

    [CoreBundle] Added OverviewListener
    * starts with system and user entries only
    * will be expanded as more comes online
    
    Signed-off-by: Jim Martens <github@2martens.de>

---
+++ src/TwoMartens/Bundle/CoreBundle/EventListener/OverviewListener.php
@@ -0,0 +1,59 @@
+<?php
+
+/*
+ * (c) Jim Martens <github@2martens.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace TwoMartens\Bundle\CoreBundle\EventListener;
+
+use TwoMartens\Bundle\CoreBundle\Event\OverviewEvent;
+
+/**
+ * The listener adds the 2WP Core Bundle links to the overview pages.
+ *
+ * @author    Jim Martens <github@2martens.de>
+ * @copyright 2013-2015 Jim Martens
+ */
+class OverviewListener
+{
+    /**
+     * Adds entries to the system overview.
+     *
+     * @param OverviewEvent $event
+     */
+    public function onSystem(OverviewEvent $event)
+    {
+        $event->addCategory('acp.system.configuration', 'TwoMartensCoreBundle');
+        $event->addEntry(
+            'acp.system.configuration',
+            'acp.options',
+            'TwoMartensCoreBundle',
+            'acp.system.options'
+        );
+    }
+
+    /**
+     * Adds entries to the user overview.
+     *
+     * @param OverviewEvent $event
+     */
+    public function onUser(OverviewEvent $event)
+    {
+        $event->addCategory('acp.user.group', 'TwoMartensCoreBundle');
+        $event->addEntry(
+            'acp.user.group',
+            'acp.group.add',
+            'TwoMartensCoreBundle',
+            'acp.user.group.add'
+        );
+        $event->addEntry(
+            'acp.user.group',
+            'acp.group.list',
+            'TwoMartensCoreBundle',
+            'acp.user.group.list'
+        );
+    }
+}
