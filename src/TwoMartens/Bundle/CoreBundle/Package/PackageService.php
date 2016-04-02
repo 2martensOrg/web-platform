511386595d15d3c1d54134a5f8d64a47a91388fb
Jim Martens <github@2martens.de>
2016-04-02 15:55:48 +0200

    [CoreBundle] WIP: Added skeleton of PackageService
    Signed-off-by: Jim Martens <github@2martens.de>

---
+++ src/TwoMartens/Bundle/CoreBundle/Package/PackageService.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+ * (c) Jim Martens <github@2martens.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace TwoMartens\Bundle\CoreBundle\Package;
+
+/**
+ * Implementation for the PackageServiceInterface.
+ *
+ * @author    Jim Martens <github@2martens.de>
+ * @copyright 2013-2016 Jim Martens
+ */
+class PackageService implements PackageServiceInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function installPackage($composerName, $versionCondition)
+    {
+        // TODO: Implement installPackage() method.
+    }
+}
